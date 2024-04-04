d47a03c3acf0768c149762a15d22884845983115
DrRaduga <jonking_95@mail.ru>
2024-04-04 15:52:45 +0300

    skript

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const calc = document.querySelector('.calc');
 const result = document.querySelector('#result');
-
+let lastBtnIsSimbol = false ;
 calc.addEventListener('click', function ({ target }) {
     const { className, innerText } = target
 
     if (!className.includes('calc__btn')) return;
-
-    const value = innerText;
-
-    switch (value) {
+    if (lastBtnIsSimbol && className.includes('calc__btn_operator')) return result.innerText = result.innerText.substring(0, result.innerText.length - 1) + innerText;
+    
+    switch (innerText) {
         case 'C':
             result.innerText = '';
             break;
@@ -17,8 +16,10 @@ calc.addEventListener('click', function ({ target }) {
             result.innerText = eval(result.innerText).toFixed(2)
             break;
 
+
         default:
-            result.innerText += value;
+            result.innerText += innerText;
     }
+    lastBtnIsSimbol = className.includes('calc__btn_operator')
 
 });
